42093ec11afe616a4d8c5fb190c41605a765cb9f
rob.arlow <rob.arlow@hightownha.org.uk>
2023-01-31 17:00:40 +0000

    Updates to Colours

---
+++ PrismBase/PrismBase.Core/Generics/AppColours.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PrismBase.Core.Generics
+{
+    public static class AppColours
+    {
+        public static SolidColorBrush Background = new SolidColorBrush(Color.FromRgb(132, 189, 0));
+        public static SolidColorBrush ButtonBackground = new SolidColorBrush(Color.FromRgb(208, 223, 0));
+        public static SolidColorBrush ButtonBorder = new SolidColorBrush(Color.FromRgb(77, 143, 170));
+        public static SolidColorBrush ButtonText = new SolidColorBrush(Color.FromRgb(0, 0, 0)); 
+        public static SolidColorBrush TabBorder = new SolidColorBrush(Color.FromRgb(117, 59, 189)); 
+    }
+}
